1ef6b648d272920033aa4bac8e0e05ab2c1517a4
Brian Wang <brwng902@gmail.com>
2025-02-19 11:32:27 -0500

    added and changed a couple tests for the student tests

--- 4-ShellP2/bats/student_tests.sh
+++ 4-ShellP2/bats/student_tests.sh
@@ -4,10 +4,54 @@
 # 
 # Create your unit tests suit in this file
 
-@test "Example: check ls runs without errors" {
+@test "Example: check ls runs without errors (external command dsh2> runs after)" {
+    current=$(pwd)
     run ./dsh <<EOF                
 ls
 EOF
+ 
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="batsdragon.cdshdsh_cli.cdshlib.cdshlib.hmakefilequestions.mddsh2>dsh2>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+@test "Check cd actually changes the directory (built-in command) {
+    current=$(pwd)
+    run ./dsh <<EOF                
+cd bats
+pwd
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="dsh2>dsh2>$current/batsdsh2>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
 
     # Assertions
     [ "$status" -eq 0 ]
