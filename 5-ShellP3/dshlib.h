f2ee11b4e6bab36324afed58080e81b4f9cc546d
Brian Wang <brwng902@gmail.com>
2025-02-28 12:43:24 -0500

    modified the files for pt 3 of shell

--- 5-ShellP3/dshlib.h
+++ 5-ShellP3/dshlib.h
@@ -7,9 +7,16 @@
 #define ARG_MAX 256
 #define CMD_MAX 8
 #define CMD_ARGV_MAX (CMD_MAX + 1)
+
 #define DIRECTORY_LENGTH 1000
 // Longest command that can be read from the shell
 #define SH_CMD_MAX EXE_MAX + ARG_MAX
+
+typedef struct command
+{
+    char exe[EXE_MAX];
+    char args[ARG_MAX];
+} command_t;
 
 typedef struct cmd_buff
 {
@@ -28,14 +35,19 @@ typedef struct command{
 }command_t;
 */
 
+typedef struct command_list{
+    int num;
+    cmd_buff_t commands[CMD_MAX];
+}command_list_t;
 
 //Special character #defines
 #define SPACE_CHAR  ' '
 #define PIPE_CHAR   '|'
 #define PIPE_STRING "|"
 
-#define SH_PROMPT "dsh2> "
+#define SH_PROMPT "dsh3> "
 #define EXIT_CMD "exit"
+#define EXIT_SC     99
 
 //Standard Return Codes
 #define OK                       0
@@ -47,7 +59,6 @@ typedef struct command{
 #define ERR_EXEC_CMD            -6
 #define OK_EXIT                 -7
 
-//Defining my own const return Code for dragon
 #define DRAGON                  -8
 
 //prototypes
@@ -55,6 +66,9 @@ int alloc_cmd_buff(cmd_buff_t *cmd_buff);
 int free_cmd_buff(cmd_buff_t *cmd_buff);
 int clear_cmd_buff(cmd_buff_t *cmd_buff);
 int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff);
+int close_cmd_buff(cmd_buff_t *cmd_buff);
+int build_cmd_list(char *cmd_line, command_list_t *clist);
+int free_cmd_list(command_list_t *cmd_lst);
 char *rightTrim(char *cmd);
 char *leftTrim(char *cmd);
 void commandSplitter(char *cmd, cmd_buff_t *cmdBuff, int cmdNum);
@@ -68,7 +82,6 @@ typedef enum {
     BI_CMD_CD,
     BI_NOT_BI,
     BI_EXECUTED,
-    BI_RC,
 } Built_In_Cmds;
 Built_In_Cmds match_command(const char *input); 
 Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd);
@@ -76,6 +89,7 @@ Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd);
 //main execution context
 int exec_local_cmd_loop();
 int exec_cmd(cmd_buff_t *cmd);
+int execute_pipeline(command_list_t *clist);
 
 
 
